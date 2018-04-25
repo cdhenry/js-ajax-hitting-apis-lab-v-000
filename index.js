8d9327b9b75b84d58a0f527f948044080c36094b
Chris Henry <cdhenry@gmail.com>
2018-04-25 23:06:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getRepositories() {
   let username = document.getElementById("username").value
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/:username/repos')
+  req.open("GET", "https://api.github.com/users/#{username}/repos")
   req.send()
 }
