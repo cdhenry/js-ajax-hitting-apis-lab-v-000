f6c46e2182297158aa55a510f4a4bdb91f5a1abd
Chris Henry <cdhenry@gmail.com>
2018-04-26 00:35:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function getRepositories() {
 
 function displayRepositories(event, data) {
   let repos = JSON.parse(this.responseText)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(
+    r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a>'
+    ' - <a href="#" data-branch="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
@@ -28,9 +30,16 @@ function displayCommits() {
 }
 
 function getBranches() {
-
+  let username = document.getElementById("username").value
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayCommits)
+  req.open("GET", `https://api.github.com/repos/${username}/${name}/commits`)
+  req.send()
 }
 
 function displayBranches() {
-
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.author.name + ' - ' + commit.commit.message +  '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
 }
