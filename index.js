e9d1fd4c825bb9d576d04d121d7c0ddf6ff36e0a
Chris Henry <cdhenry@gmail.com>
2018-04-26 00:59:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function getCommits(el) {
 function displayCommits() {
   const commits = JSON.parse(this.responseText)
   debugger;
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.author.name + ' - ' + commit.commit.message +  '</li>').join('')}</ul>`
+  const commitsList = `<ul>${commits.map(commit => debugger; '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.author.name + ' - ' + commit.commit.message +  '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
 }
 
